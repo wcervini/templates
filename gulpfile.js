aeb9e7d3773dc13071c990ce6f8889624303c38d
WALTER <walter@walter-desktop.home>
2019-06-14 14:05:51 +0100

    Creacion de Gulfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,35 @@
-const { src, dest } = require('gulp');
-const gulp = require("gulp");
-const sass = require('gulp-sass')
-const rm = require('gulp-rm');
-const css = ["./css/**/*.css","./css/**/*bootstrap"];
+const gulp        = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass        = require('gulp-sass');
+const reload      = browserSync.reload;
+const rm          = require('gulp-rm');
+const src = {
+    scss: './src/scss/**/*.scss',
+    css:  './css',
+    html: './*.html',
+    src:  './src'
+};
 
-gulp.task("sass", function(){
-    return src("./node_modules/**/mdb*.scss")
-        .pipe(sass())
-        .pipe(dest('./css'));
-});
 
-gulp.task("clear",function(){
-    return src(css)
-        .pipe(rm());
-});
+// Compile sass into CSS
+function style(){
+    return gulp.src(src.scss)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(src.css))
+        .pipe(reload({stream: true}));
+};
 
+function watch(){
+    browserSync.init({
+        server: {
+            baseDir: src
+        } 
+    });
+    gulp.watch(src.scss,style);
+    gulp.watch(src.html).on("change",reload);    
+}
+
+
+exports.sass= style;
+exports.watch= watch;
+exports.default=watch;
